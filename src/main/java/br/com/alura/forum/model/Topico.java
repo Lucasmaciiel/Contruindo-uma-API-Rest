40fba612f8a49623979626428a7dcd064e2437bb
Lucas Maciel <lucasmacielgois@gmail.com>
2020-06-05 17:15:16 -0300

    Validações com Bean Validation

--- src/main/java/br/com/alura/forum/model/Topico.java
+++ src/main/java/br/com/alura/forum/model/Topico.java
@@ -26,8 +26,11 @@ public class Topico {
 	
 	
 	
-	public Topico(String titulo, String mensagem, Curso curso) {
+	public Topico() {
 		super();
+	}
+
+	public Topico(String titulo, String mensagem, Curso curso) {
 		this.titulo = titulo;
 		this.mensagem = mensagem;
 		this.curso = curso;
