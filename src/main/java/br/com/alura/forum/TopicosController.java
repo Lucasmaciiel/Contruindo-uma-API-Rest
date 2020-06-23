ce3d0c63e64166aa3859132288bdef99c5619640
Lucas Maciel <lucasmacielgois@gmail.com>
2020-06-23 16:11:17 -0300

    Paginação de tópicos

--- src/main/java/br/com/alura/forum/TopicosController.java
+++ src/main/java/br/com/alura/forum/TopicosController.java
@@ -9,6 +9,9 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -44,12 +48,15 @@ public class TopicosController {
 
 	// ANTES @RequestMapping(value="/topicos", method = RequestMethod.GET)
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,@RequestParam int pagina,@RequestParam int qtd) {
+		
+		Pageable paginacao = PageRequest.of(pagina, qtd);
+		
 		if (nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos);
 		} else {
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 			return TopicoDto.converter(topicos);
 		}
 	}
--- src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+
 import br.com.alura.forum.model.Topico;
 
 public class TopicoDto {
@@ -34,8 +36,8 @@ public class TopicoDto {
 	}
 	
 	// Converte tópico para tópicoDto
-	public static List<TopicoDto> converter(List<Topico> topicos) {
-		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+	public static Page<TopicoDto> converter(Page<Topico> topicos) {
+		return topicos.map(TopicoDto::new);
 	}
 
 	
