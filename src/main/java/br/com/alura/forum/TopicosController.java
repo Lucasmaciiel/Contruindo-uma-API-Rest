119f9707487ad55d205814269622cc63e8865bc6
Lucas Maciel <lucasmacielgois@gmail.com>
2020-06-26 11:13:00 -0300

    Invalidando o Cache, no cadastrar, deletar, atualizar

--- src/main/java/br/com/alura/forum/TopicosController.java
+++ src/main/java/br/com/alura/forum/TopicosController.java
@@ -9,6 +9,8 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -47,8 +49,8 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 
-	// ANTES @RequestMapping(value="/topicos", method = RequestMethod.GET)
 	@GetMapping
+	@Cacheable(value = "listaDeTopicos")
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
 			Pageable paginacao) {
 		
@@ -63,6 +65,7 @@ public class TopicosController {
 
 	@PostMapping
 	@Transactional
+	@CacheEvict(value= "listaDeTopicos", allEntries=true) // invalida determinado cache, e limpa todos os registros
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -82,6 +85,7 @@ public class TopicosController {
 	
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value= "listaDeTopicos", allEntries=true) 
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if(optional.isPresent()){
@@ -93,6 +97,7 @@ public class TopicosController {
 	
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value= "listaDeTopicos", allEntries=true) 
 	public ResponseEntity<?> remover(@PathVariable Long id){
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if(optional.isPresent()){
