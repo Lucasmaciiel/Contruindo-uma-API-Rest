1d31270a9688100a3f1d5a546ac42f434ed34856
Lucas Maciel <lucasmacielgois@gmail.com>
2020-07-09 18:07:46 -0300

    Autenticando o Cliente via springSecurity

--- src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfigurations.java
@@ -17,6 +17,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import br.com.alura.forum.repository.UsuarioRepository;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
@@ -26,6 +28,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	private AutenticacaoService autenticacaoService;
 	@Autowired
 	private TokenService tokenService;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
 	@Override
 	@Bean
@@ -49,7 +53,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 		.anyRequest().authenticated()
 		.and().csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
+		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	//Configurações recursos staticos(css, js, img)
